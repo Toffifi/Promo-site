d85ebed6fc03146e29952ede8ee09f246d6a4726
Toffifi <sanyq@mail.ru>
2020-03-21 22:24:01 +0300

    feat: add portfolio interaction

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+const portfolioTabs = document.querySelectorAll('.portfolio ul li');
+const img = document.querySelectorAll('.gallery img');
+
 document.addEventListener('scroll', onScroll);
 
 function onScroll(e) {
@@ -8,9 +11,32 @@ function onScroll(e) {
             links.forEach((a) => {
                 a.classList.remove('active');
                 if (el.getAttribute('id') === a.getAttribute('href').substring(1)) {
-                    a.classList.add('active')
+                    a.classList.add('active');
                 }
             })
         }
     })
 }
+
+portfolioTabs.forEach((el) => {
+    el.addEventListener('click', (e) => {      
+        const img = document.querySelector('.gallery');  
+        for (let i = img.children.length; i >= 0; i--) {
+            img.appendChild(img.children[Math.random() * i | 0]);
+        }
+        portfolioTabs.forEach((a) => {
+            a.classList.remove('portfolioActive');
+        })
+        e.target.classList.add('portfolioActive');
+
+    })
+})
+
+img.forEach((el) => {
+    el.addEventListener('click', (e) => {
+        img.forEach((a) => {
+            a.classList.remove('activeImg');
+        })
+        e.target.classList.add('activeImg');
+    })
+})
